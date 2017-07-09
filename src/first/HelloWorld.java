e4c9c9f997209f6206eb387f1e827100a2552fcb
tianjiachengde <729765717@qq.com>
2017-07-09 14:45:41 +0800

    how are you

--- src/first/HelloWorld.java
+++ src/first/HelloWorld.java
@@ -3,8 +3,8 @@ package first;
 public class HelloWorld {
 
 	public static void main(String[] args){
-		System.out.println("Hello World!");
-		
+		System.out.println("Hello World!");	
+		System.out.println("How are you?");	
 		System.out.println("i'm fine thanks");
 	}
 }
